6ef94f5cb8ce415e96b165d30143a2642795852c
Angelo Aad <angelo.aad1@gmail.com>
2025-07-07 10:09:47 +0300

    feat: implement shared Infinite Scrolling component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { httpBatchLink } from "@trpc/client";
 import { env } from "./lib/utils/env";
 import { trpc } from "./lib/utils/trpc";
 import { ExperienceList } from "./features/experiences/components/ExperienceList";
+import { InfiniteScroll } from "./features/shared/components/InfiniteScroll";
 
 export function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -49,11 +50,20 @@ export function App() {
 }
 
 function Index() {
-  const experiencesQuery = trpc.experiences.feed.useQuery({});
+  const experiencesQuery = trpc.experiences.feed.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
   return (
-    <ExperienceList
-      experiences={experiencesQuery.data?.experiences ?? []}
-      isLoading={experiencesQuery.isLoading}
-    />
+    <InfiniteScroll onLoadMore={experiencesQuery.fetchNextPage}>
+      <ExperienceList
+        experiences={
+          experiencesQuery.data?.pages.flatMap((page) => page.experiences) ?? []
+        }
+        isLoading={experiencesQuery.isLoading || experiencesQuery.isFetchingNextPage}
+      />
+    </InfiniteScroll>
   );
 }
